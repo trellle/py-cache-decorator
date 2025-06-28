b7e2637532d56b11593e56c980718d112d4ffe76
yarik <super.racing@ukr.net>
2025-06-28 10:32:48 +0300

    refactored my code

--- app/main.py
+++ app/main.py
@@ -5,23 +5,14 @@ def cache(func: Callable) -> Callable:
     my_store = {}
 
     def wrapper(*args) -> Any:
-        flag = True
-        for arg in args:
-            if isinstance(arg, (list, dict, set, Callable)):
-                flag = False
-                break
-        if flag:
-            if str(func) in my_store and str(args) not in my_store[str(func)]:
-                print("Calculating new result")
-                result = func(*args)
-                my_store[str(func)].update({str(args): result})
-                return result
-            elif str(func) in my_store and str(args) in my_store[str(func)]:
-                print("Getting from cache")
-                return my_store[str(func)][str(args)]
-            elif str(func) not in my_store:
-                print("Calculating new result")
-                result = func(*args)
-                my_store.update({str(func): {str(args): result}})
-                return result
+        if str(func) in my_store and str(args) in my_store[str(func)]:
+            print("Getting from cache")
+            return my_store[str(func)][str(args)]
+        print("Calculating new result")
+        result = func(*args)
+        if str(func) in my_store and str(args) not in my_store[str(func)]:
+            my_store[str(func)].update({str(args): result})
+        else:
+            my_store.update({str(func): {str(args): result}})
+        return result
     return wrapper
