385173f262d7a5a4226dd0ba7923ad087b6f953e
yarik <super.racing@ukr.net>
2025-07-02 16:35:34 +0300

    removed str conversion

--- app/main.py
+++ app/main.py
@@ -5,14 +5,14 @@ def cache(func: Callable) -> Callable:
     my_store = {}
 
     def wrapper(*args) -> Any:
-        if str(func) in my_store and str(args) in my_store[str(func)]:
+        if func in my_store and args in my_store[func]:
             print("Getting from cache")
-            return my_store[str(func)][str(args)]
+            return my_store[func][args]
         print("Calculating new result")
         result = func(*args)
-        if str(func) in my_store and str(args) not in my_store[str(func)]:
-            my_store[str(func)].update({str(args): result})
+        if func in my_store and args not in my_store[func]:
+            my_store[func].update({args: result})
         else:
-            my_store.update({str(func): {str(args): result}})
+            my_store.update({func: {args: result}})
         return result
     return wrapper
