b1991ea9251dce65e0050212fac1a70f0e617939
yarik <super.racing@ukr.net>
2025-07-03 14:13:28 +0300

    simplified the decorator

--- app/main.py
+++ app/main.py
@@ -5,14 +5,11 @@ def cache(func: Callable) -> Callable:
     my_store = {}
 
     def wrapper(*args) -> Any:
-        if func in my_store and args in my_store[func]:
+        if args in my_store:
             print("Getting from cache")
-            return my_store[func][args]
+            return my_store[args]
         print("Calculating new result")
         result = func(*args)
-        if func in my_store and args not in my_store[func]:
-            my_store[func].update({args: result})
-        else:
-            my_store.update({func: {args: result}})
+        my_store[args] = result
         return result
     return wrapper
